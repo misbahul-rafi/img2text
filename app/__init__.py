3b4f195c62b502003f50a1f4f2934eb9d81d63e3
misbahul-rafi <misbahulrafi24@gmail.com>
2024-10-05 16:07:58 +0000

    First Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-# app/__init__.py
 from flask import Flask
 from app.config import Config
 from app.routes.image_routes import image_routes
@@ -8,7 +7,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Buat folder unggahan jika belum ada
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
     app.register_blueprint(image_routes)
